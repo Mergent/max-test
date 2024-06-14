c57fa775312ef0946240d804ba01e00f1c8f6c5c
Seryoga <m.a.semizhonov@gmail.com>
2024-06-14 17:01:56 +0700

    choto sdelal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {createBrowserRouter, Link, RouterProvider} from "react-router-dom";
+import Wtf from "./pages/Wtf";
+import MainPage from "./Pages/MainPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/Wtf",
+    element: <div>KTO</div>,
+  },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  return (
+    <BrowseRouter>
+    <div>
+      <div>
+        <Link to="/Wtf">КТО</Link>
+        <Link to="/MainPage">ГДЕ</Link>
+      </div>
+    </div>
+      <Route path="/Wtf">
+        <Wtf/>
+      </Route>
+      <Route path="/MainPage">
+        <MainPage/>
+      </Route>
+    </BrowseRouter>
+      
+    
+  );
+
+}
 export default App;
