bec05839e8b828564dd786febf24bcb3a65aadca
Seryoga <m.a.semizhonov@gmail.com>
2024-06-29 16:34:17 +0700

    sortirovka

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
-import {createBrowserRouter, Link, RouterProvider} from "react-router-dom";
-import Wtf from "./pages/Wtf";
-import MainPage from "./pages/MainPage"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainPage from "./pages/MainPage";
 
 const router = createBrowserRouter([
   {
@@ -14,20 +13,6 @@ function App() {
 
   return (
     <RouterProvider router={router} />
-    // <BrowseRouter>
-    // <div>
-    //   <div>
-    //     <Link to="/Wtf">КТО</Link>
-    //     <Link to="/MainPage">ГДЕ</Link>
-    //   </div>
-    // </div>
-    //   <Route path="/Wtf">
-    //     <Wtf/>
-    //   </Route>
-    //   <Route path="/MainPage">
-    //     <MainPage/>
-    //   </Route>
-    // </BrowseRouter>
   );
 
 }
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,85 +1,130 @@
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
-import { Row, Col, Pagination } from "antd";
+import { Row, Col, Pagination, message } from "antd";
 import { Header } from "antd/es/layout/layout";
-// import Table from './components/UI/Table';
 import { Table } from "antd";
 import axios from 'axios';
-import { noop } from "antd/es/_util/warning";
+import Column from "antd/es/table/Column";
+import e from "cors";
+
+const MySelect = ({ options, defaultValue, value, onChange }) => {
+  return (
+    <select value={value}
+      onChange={event => onChange(event.target.value)}
+      
+    >
+      <option  disabled={true} value=''>{defaultValue}</option>
+      {options?.map(option =>
+        <option  key={option.name} value={option.name}>
+          {option.value}
+        </option>
+      )}
+
+    </select>
+  );
+};
+
+const columns = [
+  {
+    title: 'Username',
+    dataIndex: 'username',
+    key: 'username',
+    filterDropdown: true,
+    sorter: true,
+  },
+  {
+    title: 'E-mail',
+    dataIndex: 'email',
+    key: 'email',
+    sorter: true,
+  },
+  {
+    title: 'First name',
+    dataIndex: 'firstName',
+    key: 'firstName',
+    sorter: true, 
+  },
+  {
+    title: 'Last name',
+    dataIndex: 'lastName',
+    key: 'lastName',
+    sorter: true,
+  },
+  {
+    title: 'Role',
+    dataIndex: 'role',
+    key: 'role',
+    sorter: true,
+  },
+];
+// const array=(extra)=>{console.log(array)}
+
+
+
 
 
 function MainPage() {
-  const [columns, setColumns]= useState([
-    {
-        title: 'Username',
-        dataIndex: 'username',
-        key:'username',
-    },
-    {
-        title: 'E-mail',
-        dataIndex: 'email',
-        key:'email',
-    },
-    {
-        title: 'First name',
-        dataIndex: 'firstName',
-        key:'firstName',
-    },
-    {
-        title: 'Last name',
-        dataIndex: 'lastName',
-        key:'lastName',
-    },
-    {
-        title: 'Role',
-        dataIndex: 'role',
-        key:'role',
-    },
-  ]);
 
-  const[dataSource, setDataSource] = useState([]);
+  const onChange = (pagination, sorter, extra) => { 
+    // return extra === objec;
+};
 
+  // const objec= { toString: function() { return this.field + ", " + this.order; } };
+  
+
+  const [dataSource, setDataSource] = useState([]);
   const [page, setPage] = useState(0)
-
-  useEffect(()=>{
-    axios
-      .get('http://localhost:6100/users', {params:{page: page, sort:'email,asc', size: 10}})
-      .then(data =>{
+  const [selectedSort, setSelectedSort] = useState('')
+  const [sortParams, setSortParams] = useState()
+  
+  const getUsers = async () => {
+    await axios
+      .get('http://localhost:6100/users', { params: { page: page, sort:sortParams , size: 10 } })
+      .then(data => {
         setDataSource(data.data.content);
+        
       })
-  }, [page]);
-  // const [dataSource, setDataSource] = useState ([]);
-  // useEffect(()=>{
-  //   fetch('http://localhost:6100/users', {
-  //     method:'GET',
-  //   })
-  //   .then(res => res.json())
-  //   .then((result) =>{
-  //     setDataSource(result.component);
-  //   });
-  // },[]);
+  };
 
+  useEffect(() => {
+    getUsers();
+    
+  }, [page, sortParams]);
 
   return (
     <div className="App">
+      <div>
+        <MySelect
+          
+          value={selectedSort}
+          onChange={setSelectedSort}
+          defaultValue='Сортировка'
+          options={[
+            { value: 'Username', name: 'username' },
+            { value: 'Email', name: 'email, desc' },
+            { value: 'Firstname', name: 'firstname' },
+            { value: 'Lastname', name: 'lastname' },
+            { value: 'Role', name: 'role' },
+
+          ]}
+        />
+      </div>
+
       <Header />
-      <Row>
-      <Col xs={24} md={{ span: 12, offset: 6 }}>
-        <Table
-          dataSource={dataSource}
-          columns={columns}
-          pagination={false}
-        />
-        <Pagination 
-          total={111}
-          onChange={(data) => {
-            console.log(data)
-            setPage(data - 1)
-          }}
-        />
-        </Col>
-      </Row>
+      <Table
+        onChange={setSortParams}
+        dataSource={dataSource}
+        columns={columns}
+        pagination={false}
+        filterDropdown={true}
+      />
+      <Pagination
+        total={111}
+        onChange={(data) => {
+          setPage(data - 1)
+        }}
+      />
     </div>
   );
 
