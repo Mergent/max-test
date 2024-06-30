7ade84982e15b0aa26a2236686c58807ad4a4a0e
Seryoga <m.a.semizhonov@gmail.com>
2024-06-30 23:50:32 +0700

    added page size for pagination, removed unnecessary code

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,29 +1,8 @@
 import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
-import { Row, Col, Pagination, message } from "antd";
+import { useEffect, useState} from "react"; 
 import { Header } from "antd/es/layout/layout";
 import { Table } from "antd";
 import axios from 'axios';
-import Column from "antd/es/table/Column";
-import e from "cors";
-
-const MySelect = ({ options, defaultValue, value, onChange }) => {
-  return (
-    <select value={value}
-      onChange={event => onChange(event.target.value)}
-      
-    >
-      <option  disabled={true} value=''>{defaultValue}</option>
-      {options?.map(option =>
-        <option  key={option.name} value={option.name}>
-          {option.value}
-        </option>
-      )}
-
-    </select>
-  );
-};
 
 const columns = [
   {
@@ -43,7 +22,7 @@ const columns = [
     title: 'First name',
     dataIndex: 'firstName',
     key: 'firstName',
-    sorter: true, 
+    sorter: true,
   },
   {
     title: 'Last name',
@@ -58,83 +37,46 @@ const columns = [
     sorter: true,
   },
 ];
-// const array=(extra)=>{console.log(array)}
-
-
-
-
 
 function MainPage() {
-
-  const onChange = (pagination, sorter, extra) => { 
-    // return extra === objec;
-
-    // const objec= { toString: function() { return this.field + ", " + this.order; } };
-  };
-  
-
   const [dataSource, setDataSource] = useState([]);
   const [page, setPage] = useState(0)
-  const [selectedSort, setSelectedSort] = useState('')
   const [sortParams, setSortParams] = useState('email,asc')
+  const [sizeOfPage, setsizeOfPage] = useState(10)
 
   useEffect(() => {
-    console.log('StateSortParams => ', sortParams)
-  }, [sortParams])
-  
+    console.log('State: sizeOfPage, SortParams => ', sizeOfPage, sortParams)
+  }, [sizeOfPage, sortParams])
+
   const getUsers = async () => {
     await axios
-      .get('http://localhost:6100/users', { params: { page: page, sort: sortParams, size: 10 } })
+      .get('http://localhost:6100/users', { params: { page: page, sort: sortParams, size: sizeOfPage } })
       .then(data => {
         setDataSource(data.data);
       })
   };
-
+  
   useEffect(() => {
     getUsers();
-  }, [sortParams, page]);
+  }, [sortParams, page, sizeOfPage]);
 
   return (
     <div className="App">
-      <div>
-        <MySelect
-          
-          value={selectedSort}
-          onChange={setSelectedSort}
-          defaultValue='Сортировка'
-          options={[
-            { value: 'Username', name: 'username' },
-            { value: 'Email', name: 'email, desc' },
-            { value: 'Firstname', name: 'firstname' },
-            { value: 'Lastname', name: 'lastname' },
-            { value: 'Role', name: 'role' },
-
-          ]}
+      <Header/>
+        <Table
+          onChange={(pagination, filters, sorter, extra) => {
+            console.log("LOG -> ~ MainPage ~ pagination:", pagination)
+            setsizeOfPage(pagination.pageSize)
+            setPage(pagination.current - 1)
+            setSortParams(`${sorter.field},${sorter.order?.slice(0, -3) ?? 'asc'}`)
+          }}
+          dataSource={dataSource.content}
+          columns={columns}
+          pagination={{
+            total: dataSource.totalElements
+          }} 
         />
-      </div>
-
-      <Header />
-      <Table
-        onChange={(pagination, filters, sorter, extra) => {
-          console.log("LOG -> ~ MainPage ~ pagination:", pagination)
-          setPage(pagination.current - 1)
-          setSortParams(`${sorter.field},${sorter.order?.slice(0, -3) ?? 'asc'}`)
-        }}
-        dataSource={dataSource.content}
-        columns={columns}
-        pagination={{
-          total: dataSource.totalElements
-        }}
-        filterDropdown={true}
-      />
-      {/* <Pagination
-        total={111}
-        onChange={(data) => {
-          setPage(data - 1)
-        }}
-      /> */}
     </div>
   );
-
 }
 export default MainPage;
