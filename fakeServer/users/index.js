accb817191059db5d8066c116c5c7f99ba06b616
Mergent <makachino93@gmail.com>
2024-07-10 10:33:35 +0700

    fakeServer add endpoint /register

--- fakeServer/users/index.js
+++ fakeServer/users/index.js
@@ -3,7 +3,7 @@ const { jwtDecode } = require('jwt-decode');
 
 const { v4: uuidv4 } = require('uuid');
 
-const { readFile, filteredAndPaginationSort } = require("../utils.js");
+const { readFile, filteredAndPaginationSort, pushElem } = require("../utils.js");
 
 const usersPath = './users/data.json'
 const router = express.Router();
@@ -12,6 +12,20 @@ router.get('/users', (req, res) => {
   const users = readFile(usersPath)
   const params = req.query
   res.send(filteredAndPaginationSort(users, params))
+});
+
+router.get('/register', (req, res) => {
+  const users = readFile(usersPath)
+  const { body } = req
+
+  if (users.map(u => u.username).includes(body.username)) {
+    res.status(403).send({ error: `User with username ${body.username} already exist` })
+    return;
+  }
+
+  const newUser = pushElem(users, body, usersPath)
+
+  res.send(newUser)
 });
 
 module.exports = router;
